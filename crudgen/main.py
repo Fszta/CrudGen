d70d874211257a86e7e8062063d51f1fa3d8549d
fszta <antoinefer@hotmail.com>
2020-11-18 22:09:16 +0100

    Init

---
+++ crudgen/main.py
@@ -0,0 +1,26 @@
+from crudgen.schema.schema_generator import SchemaGenerator
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test_fields = {
+        "schema_field": [
+            {
+                "field_name": "name",
+                "field_type": "str"
+            },
+            {
+                "field_name": "age",
+                "field_type": "int"
+            },
+            {
+                "field_name": "img_url",
+                "field_type": "str"
+            },
+            {
+                "field_name": "description",
+                "field_type": "str"
+            }
+        ]
+    }
+    test = SchemaGenerator("test", test_fields)
+    test.run()
