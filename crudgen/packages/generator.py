5c852d10c1951aff351c38bed6ee1d196d231b2e
fsz <antoinefer@hotmail.com>
2020-11-19 22:10:33 +0100

    Update generator

--- crudgen/packages/generator.py
+++ crudgen/packages/generator.py
@@ -13,15 +13,19 @@ def create_api_structure():
     [create_package(package, False) for package in PACKAGES]
 
 
-def create_package(directory_name: str, is_base_dir: bool):
-    """ Create python package if not exists """
+def create_package(package_name: str, is_base: bool):
+    """
+    Create python package if not exists
 
-    if not is_base_dir:
-        directory_path = BASE + "/" + directory_name
+    :param package_name: name of the package dir
+    :param is_base: bool - true is package is base (higher level)
+    """
+    if not is_base:
+        directory_path = BASE + "/" + package_name
     else:
-        directory_path = directory_name
+        directory_path = package_name
 
     if not os.path.exists(directory_path):
         os.mkdir(directory_path)
-        os.mknod(os.path.join(directory_path, "__init__.py"))
-
+        with open(directory_path+"/__init__.py", "w"):
+            pass
--- crudgen/tests/packages/test_generator.py
+++ crudgen/tests/packages/test_generator.py
@@ -15,3 +15,8 @@ class TestPackageGenerator(unittest.TestCase):
         # Check test_package contains __init__.py
         files_in_test_package = os.listdir(test_package)
         self.assertTrue("__init__.py" in files_in_test_package)
+
+    def test_create_api_structure(self):
+        generator.create_api_structure()
+        files_in_generated = os.listdir("generated_api")
+        [self.assertTrue(package in files_in_generated) for package in generator.PACKAGES]
