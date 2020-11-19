8ed51b4b7a73987331ce88fcbe7c210143bf6e3c
fsz <antoinefer@hotmail.com>
2020-11-20 00:12:40 +0100

    Add testconfig

--- crudgen/utils/config.py
+++ crudgen/utils/config.py
@@ -1,2 +1,27 @@
+import os
+
+CONFIG_ENV = os.getenv("CONFIG")
+
+if CONFIG_ENV is None:
+    CONFIG_ENV = "test"
+
+
 class Config:
-    CONFIG_TYPE = "dev"
+    GENERATED_API_PATH = "generated_api"
+    SCHEMA_PACKAGE_PATH = GENERATED_API_PATH + "/schema/"
+
+
+class DevConfig(Config):
+    TYPE = ""
+
+
+class TestConfig(Config):
+    CONFIG_ENV = "test"
+    GENERATED_API_PATH = "generated_api"
+    SCHEMA_PACKAGE_PATH = ""
+
+
+config = dict(
+    dev=DevConfig,
+    test=TestConfig
+)
