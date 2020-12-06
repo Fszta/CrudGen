fb806d9c6517ab8d45662bc8126297ef89d52dfe
fsz <antoinefer@hotmail.com>
2020-12-06 19:11:02 +0100

    Update path in conf

--- crudgen/utils/config.py
+++ crudgen/utils/config.py
@@ -8,26 +8,25 @@ if CONFIG_ENV is None:
 
 class Config:
     VERSION = "v0.1-alpha"
-    GENERATED_API_PATH = "generated_api"
-    SCHEMA_PACKAGE_PATH = GENERATED_API_PATH + "/schema/"
-    MODEL_PACKAGE_PATH = GENERATED_API_PATH + "/model/"
-    DATABASE_PACKAGE_PATH = GENERATED_API_PATH + "/database/"
-    ROUTER_PACKAGE_PATH = GENERATED_API_PATH + "/router/"
-    CONTROLLER_PACKAGE_PATH = GENERATED_API_PATH + "/controller/"
 
 
 class DevConfig(Config):
-    TYPE = ""
+    GENERATED_API_PATH = "generated_api"
+    SCHEMA_PACKAGE_PATH = "/schema/"
+    MODEL_PACKAGE_PATH = "/model/"
+    DATABASE_PACKAGE_PATH = "/database/"
+    ROUTER_PACKAGE_PATH = "/router/"
+    CONTROLLER_PACKAGE_PATH = "/controller/"
 
 
 class TestConfig(Config):
     CONFIG_ENV = "test"
     GENERATED_API_PATH = "generated_api"
-    SCHEMA_PACKAGE_PATH = "test/test_schema/"
-    MODEL_PACKAGE_PATH = "test/test_model/"
-    DATABASE_PACKAGE_PATH = "test/test_database/"
-    ROUTER_PACKAGE_PATH = "test/test_router/"
-    CONTROLLER_PACKAGE_PATH = "test/test_controller/"
+    SCHEMA_PACKAGE_PATH = "test/schema/"
+    MODEL_PACKAGE_PATH = "test/model/"
+    DATABASE_PACKAGE_PATH = "test/database/"
+    ROUTER_PACKAGE_PATH = "test/router/"
+    CONTROLLER_PACKAGE_PATH = "test/controller/"
     DATA_TEST_PATH = "test/resources/"
 
 
