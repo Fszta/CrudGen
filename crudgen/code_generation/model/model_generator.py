133a7912c3ca836388a3a12b88e48c932af64569
fsz <antoinefer@hotmail.com>
2020-12-07 21:18:24 +0100

    Refactor model generator

--- crudgen/code_generation/model/model_generator.py
+++ crudgen/code_generation/model/model_generator.py
@@ -1,128 +1,101 @@
 from crudgen.utils.config import config, CONFIG_ENV
 from crudgen.code_generation.indentation import Indentator
 from crudgen.code_generation.check import is_generated
+from crudgen.code_generation.code_formatting import custom_import_declaration, class_declare
+from crudgen.code_generation.imports import format_imports, db_base_import
 
 
-class ModelGenerator:
+@is_generated(package_name="model")
+def run(table_name: str, fields: dict, output_path: str):
     """
-    SQL Alchemy model code_generation.
-    Name will be used to create model file as :
-    model_name.py & also to name the corresponding
-    table in database
+    Run model generation base on input field which
+    corresponds to a table description.
+    The output is a model_name.py file inside
+    model package
     """
 
-    def __init__(self, name, fields: dict, output_path: str):
-        self.name = name
-        self.fields = fields
-        self.filename = "{}_model.py".format(name)
-        self.file_open = open(output_path + config[CONFIG_ENV].MODEL_PACKAGE_PATH + self.filename, "a")
+    filename = f"{table_name}_model.py"
+    file_model = open(output_path + config[CONFIG_ENV].MODEL_PACKAGE_PATH + filename, "a")
 
-    def get_types(self):
-        """
-        Get set of unique type needed to be imported
-        :return: set of sql alchemy type
-        """
-        types = [field["field_type"].sql_alchemy_type_name for field in self.fields]
-        unique_types = list(set(types))
-        unique_types.sort()
+    sql_alchemy_types = get_types(fields)
+    imports = format_imports(build_sql_alchemy_import(sql_alchemy_types), db_base_import())
+    model_attributes = build_all_attributes(fields)
+    content = imports + declare_class(table_name) + set_table_name(table_name) + model_attributes
 
-        return unique_types
+    file_model.write(content)
+    file_model.close()
 
-    @staticmethod
-    def build_sql_alchemy_import(types: list):
-        """
-        Build sql alchemy types import statement
-        :param types: set of sql alchemy types
-        :return: import statement string
-        """
-        import_statement = "from sqlalchemy import Column"
-        for sql_alchemy_type in types:
-            import_statement += ", " + sql_alchemy_type
+    return filename
 
-        return import_statement
 
-    @staticmethod
-    def write_import_statements(sql_alchemy_import: str):
-        """ Write model class imports """
-        imports = sql_alchemy_import + "\n" + "from database.db_init import Base"
-        return imports
+def get_types(fields: dict):
+    """
+    Get set of unique type needed to be imported
+    :return: set of sql alchemy type
+    """
+    types = [field["field_type"].sql_alchemy_type_name for field in fields]
+    unique_types = list(set(types))
+    unique_types.sort()
 
-    def declare_class(self):
-        """
-        Generate class declaration line
-        :return: class declaration
-        """
-        class_name = self.name.capitalize()
-        class_declaration = "class " + class_name + "(Base):"
+    return unique_types
 
-        return class_declaration
 
-    def set_table_name(self):
-        """
-        Set table name field,
-        will be used as table name by sqlalchemy
-        :return: tablename field
-        """
-        set_table = '__tablename__ = "{}"'.format(self.name)
+@custom_import_declaration
+def build_sql_alchemy_import(types: list):
+    """
+    Build sql alchemy types import statement
+    :param types: set of sql alchemy types
+    :return: import statement string
+    """
+    import_statement = "from sqlalchemy import Column"
+    for sql_alchemy_type in types:
+        import_statement += ", " + sql_alchemy_type
 
-        return Indentator.IND_LEVEL_1 + set_table
+    return import_statement
 
-    @staticmethod
-    def build_attribute(table_field):
-        """
-        Build model attributes, generates :
-        - field name
-        - if field is primary key
-        - sql alchemy type
-        - if field is unique
-        :param table_field: table field description
-        """
-        if table_field["primary_key"] is True:
-            return "{} = Column({}, primary_key=True, index=True)"\
-                .format(table_field["field_name"], table_field["field_type"].sql_alchemy_type_name)
-        else:
-            return "{} = Column({}, unique={})".format(table_field["field_name"],
-                                                       table_field["field_type"].sql_alchemy_type_name,
-                                                       table_field["unique"])
 
-    def jump_lines(self, number_of_jump):
-        """
-        Jump lines in file
-        :param number_of_jump: number of line to jump, 0 = return to line
-        """
-        for i in range(0, number_of_jump):
-            self.file_open.write("\n")
+@class_declare
+def declare_class(table_name: str):
+    """
+    Generate class declaration line
+    :return: class declaration
+    """
+    class_name = table_name.capitalize()
+    class_declaration = f"class {class_name}(Base):"
 
-    @is_generated(package_name="model")
-    def run(self):
-        """
-        Run model generation base on input field which
-        corresponds to a table description.
-        The output is a model_name.py file inside
-        model package
-        """
+    return class_declaration
 
-        # Extract unique sql alchemy types
-        sql_alchemy_types = self.get_types()
 
-        # Write import statement
-        self.file_open.write(self.write_import_statements(self.build_sql_alchemy_import(sql_alchemy_types)))
-        self.jump_lines(3)
+@custom_import_declaration
+def set_table_name(table_name: str):
+    """
+    Set table name field,
+    will be used as table name by sqlalchemy
+    :return: tablename field
+    """
+    set_table = f'__tablename__ = "{table_name}"'
 
-        # Write class declaration
-        self.file_open.write(self.declare_class())
-        self.jump_lines(1)
+    return Indentator.IND_LEVEL_1 + set_table
 
-        # Set table name
-        self.file_open.write(self.set_table_name())
-        self.jump_lines(1)
 
-        # Write class content
-        for field in self.fields:
-            self.file_open.write(Indentator.IND_LEVEL_1 + self.build_attribute(field))
-            self.jump_lines(1)
+@custom_import_declaration
+def build_attribute(table_field):
+    """
+    Build model attributes, generates :
+    - field name
+    - if field is primary key
+    - sql alchemy type
+    - if field is unique
+    :param table_field: table field description
+    """
+    field_name = table_field["field_name"]
+    field_type = table_field["field_type"].sql_alchemy_type_name
 
-        # Close generated file
-        self.file_open.close()
+    if table_field["primary_key"] is True:
+        return f"{field_name} = Column({field_type}, primary_key=True, index=True)"
+    else:
+        return f"{field_name} = Column({field_type}, unique={table_field['unique']})"
 
-        return self.filename
+
+def build_all_attributes(fields: dict):
+    return "".join([Indentator.IND_LEVEL_1 + build_attribute(field) for field in fields])
--- test/model/test_model_generator.py
+++ test/model/test_model_generator.py
@@ -1,52 +1,46 @@
-import os
 from unittest import TestCase
+from test import setup
 from crudgen.utils.config import config
+from crudgen.code_generation.model import model_generator
 from test.resources.data_test import DataTest
 from test.utils.files import check_files_are_identical
-from test.model.model_generator_test import test_generator
 
 
 class TestModelGenerator(TestCase):
 
-    def test_init(self):
-        """ Test model generation init create model_table.py file inside model package """
-        files_in_dir = os.listdir(config["test"].MODEL_PACKAGE_PATH)
-
-        self.assertTrue("generated_model.py" in files_in_dir)
-
     def test_get_types(self):
         """ Test get set of unique sql alchemy type """
-        types = test_generator.get_types()
+        types = model_generator.get_types(DataTest.TEST_FIELDS)
         expected_types = ["Integer", "String"]
 
         self.assertEqual(types, expected_types)
 
     def test_build_sql_alchemy_import(self):
         """ Test sql alchemy type import generation """
-        expected_import = "from sqlalchemy import Column, Boolean, Integer, String"
+        expected_import = "from sqlalchemy import Column, Boolean, Integer, String\n"
         test_types = ["Boolean, Integer, String"]
-        generated_import = test_generator.build_sql_alchemy_import(test_types)
+        generated_import = model_generator.build_sql_alchemy_import(test_types)
 
         self.assertEqual(expected_import, generated_import)
 
     def test_declare_class(self):
         """ Test write class desclaration in model.py file """
-        expected_class_declaration = "class Generated(Base):"
-        test_class_declaration = test_generator.declare_class()
+        expected_class_declaration = "\nclass Generated(Base):\n"
+        test_class_declaration = model_generator.declare_class("generated")
 
         self.assertEqual(expected_class_declaration, test_class_declaration)
 
     def test_set_table_name(self):
         """ Test set table name """
-        expected_table_name_field = '    __tablename__ = "generated"'
-        generated_field = test_generator.set_table_name()
+        expected_table_name_field = '    __tablename__ = "generated"\n'
+        generated_field = model_generator.set_table_name("generated")
 
         self.assertEqual(expected_table_name_field, generated_field)
 
     def test_build_attribute(self):
         """Test build model attribute for sql alchemy table description"""
-        expected_attribute = "id = Column(Integer, primary_key=True, index=True)"
-        generated_attribute = test_generator.build_attribute(DataTest.TEST_FIELDS[0])
+        expected_attribute = "id = Column(Integer, primary_key=True, index=True)\n"
+        generated_attribute = model_generator.build_attribute(DataTest.TEST_FIELDS[0])
 
         self.assertEqual(expected_attribute, generated_attribute)
 
@@ -56,7 +50,7 @@ class TestModelGenerator(TestCase):
         model_test.py file should be generated in model package.
         """
         # Run model generation
-        test_generator.run()
+        model_generator.run("generated", DataTest.TEST_FIELDS, "")
 
         # Files path to compare
         generated_model_path = config["test"].MODEL_PACKAGE_PATH + "generated_model.py"
