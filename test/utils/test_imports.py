962712200b0cf1fc30d67fba5fbb2304f49889af
fsz <antoinefer@hotmail.com>
2020-12-10 00:18:52 +0100

    Update imports.py

--- test/utils/test_imports.py
+++ test/utils/test_imports.py
@@ -3,6 +3,11 @@ from crudgen.code_generation.imports import *
 
 
 class TestImports(TestCase):
+    def test_generate_uvicorn_import(self):
+        expected_import = "import uvicorn\n"
+        generated_import = uvicorn_import()
+        self.assertEqual(expected_import, generated_import)
+
     def test_generate_fastapi_import(self):
         """ Test fastapi import generation"""
         expected_import = "from fastapi import APIRouter, HTTPException, Depends" + "\n"
@@ -31,6 +36,30 @@ class TestImports(TestCase):
         """ Test database import generation """
         expected_import = "from database.db_init import get_db" + "\n"
         generated_import = db_init_import()
+        self.assertEqual(expected_import, generated_import)
+
+    def test_database_base_import(self):
+        """ Test database import when not entrypoint """
+        expected_import = "from database.db_init import Base\n"
+        genenerated_import = db_base_import(False)
+        self.assertEqual(expected_import, genenerated_import)
+
+    def test_database_entrypoint_import(self):
+        """ Test database import when not entrypoint """
+        expected_import = "from database.db_init import Base, engine\n"
+        genenerated_import = db_base_import(True)
+        self.assertEqual(expected_import, genenerated_import)
+
+    def test_routers_import(self):
+        """ Test import tables router """
+        expected_import = "from router import table_1_router, table_2_router\n"
+        generated_import = routers_import(["table_1", "table_2"])
+        self.assertEqual(expected_import, generated_import)
+
+    def test_fastapi_core_import(self):
+        """ Test generate FastApi core import """
+        expected_import = "from fastapi import FastAPI\n"
+        generated_import = fastapi_core_import()
         self.assertEqual(expected_import, generated_import)
 
     def test_format_imports(self):
