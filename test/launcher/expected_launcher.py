7bab54a6bd7099463f65bc7e368a50a97f342467
fsz <antoinefer@hotmail.com>
2020-12-20 18:08:09 +0100

    Conditional cors activation

--- test/launcher/expected_launcher.py
+++ test/launcher/expected_launcher.py
@@ -2,9 +2,19 @@ import uvicorn
 from fastapi import FastAPI
 from database.db_init import Base, engine
 from router import table_1_router, table_2_router
+from starlette.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 app.include_router(table_1_router.router)
 app.include_router(table_2_router.router)
 
